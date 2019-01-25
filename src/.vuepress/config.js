e1ab5eaaf74118c4efeaa1d2ae7d262aad13e496
task4233 <tks.m1205@gmail.com>
2019-01-25 06:25:38 +0000

    [UPDATE] src/.vuepress/config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,29 @@
 module.exports = {
   title: 'Note',
   description: 'つらつらと書いていきます',
-  dest: 'docs'
+  base: '/note/'
+  dest: 'docs',
+  head: [
+    ['link',
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'
+      }
+    ],
+    ['link',
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css'
+      }
+    ]
+  ]
+  markdown: {
+    anchor: {
+      permalink: true
+    },
+    config: md => {
+      md.use(require("markdown-it-katex"))
+      md.options.linkify = true
+    }
+  }
 }
