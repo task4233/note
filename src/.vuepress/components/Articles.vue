28e008a0764e116d6df8e87ce8c1312e10317d2d
task4233 <tks.m1205@gmail.com>
2019-01-27 01:08:44 +0000

    [ADD] todo.*

---
+++ src/.vuepress/components/Articles.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <div v-for="page in filteredPages" class="article-container" v-if="page.frontmatter.exclude">
+    <h3 class="article-title">
+      <router-link :to="page.path">{{page.title || 'No Title'}}</router-link>
+    </h3>
+    <div class="tag-container">
+      <i class="fas fa-tags tag-icon"></i>
+      <div v-for="content in page.frontmatter.category" class="tag">{{content}}</div>
+    </div>
+    <div v-if="page.frontmatter.description" class="article-description">{{page.frontmatter.description}}</div>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'Articles',
+  props: ['pages', 'prefix'],
+  computed: {
+    filteredPages () {
+      return this.pages.filter(page => page.path.includes(this.prefix || '')) 
+    }
+  }
+}
+</script>
