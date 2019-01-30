2db778f93b0b820d0040fd5f85b42ae02210e348
task4233 <tks.m1205@gmail.com>
2019-01-30 01:34:46 +0000

    [FIXED] links in list.html has been fixed #1

--- src/.vuepress/components/PostList.vue
+++ src/.vuepress/components/PostList.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <div v-for="post in posts">
-      <p>path = {{ post.path }}</p>
-      <h2><a v-bind:href="post.path">{{ post.title }} </a></h2>
+      <h2><a v-bind:href="$withBase(post.path)">{{ post.title }} </a></h2>
       <p>{{ post.frontmatter.description }}</p>
-      <a v-bind:href="post.path">続きを読む!</a>
+      <a v-bind:href="$withBase(post.path)">続きを読む!</a>
     </div>
   </div>
 </template>
