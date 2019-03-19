af0eb91ac9e235bfe818a3292511add9ba80bc81
task4233 <tks.m1205@gmail.com>
2019-03-19 10:57:29 +0000

    [UPDATE] config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,44 +1,46 @@
 module.exports = {
-  locales: {
-    '/': {
-      lang: 'ja',
-      title: 'Note',
-      description: 'Take it easy. But not lazy.'
-    }
-  },
-  base: '/note/',
-  dest: 'docs',
-  head: [
-    ['link',
-      {
-        rel: 'stylesheet',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'
-      }
-    ],
-    ['link',
-      {
-        rel: 'stylesheet',
-        href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css'
-      }
-    ]
-  ],
-  markdown: {
-    lineNumbers: true,
-    anchor: {
-      permalink: true
+    locales: {
+        '/': {
+            lang: 'ja',
+            title: 'Note',
+            description: 'Take it easy. But not lazy.'
+        }
     },
-    config: (md) => {
-      md.use(require('markdown-it-katex'))
-      md.use(require('markdown-it-task-lists'))
-      md.options.linkify = true
-    }
-  },
-  themeConfig: {
-    nav: [
-      { text: 'Todo'           , link: '/todo.html' },
-      { text: 'Chukapi-Fun-Art', link: '/chukapi_fun_art.html' },
-      { text: 'Articles'       , link: '/list.html' }
+    base: '/note/',
+    dest: 'docs',
+    head: [
+        ['link',
+         {
+             rel: 'stylesheet',
+             href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'
+         }
+        ],
+        ['link',
+         {
+             rel: 'stylesheet',
+             href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css'
+         }
+        ]
     ],
-    search: true,
-  }
+    markdown: {
+        lineNumbers: true,
+        anchor: {
+            permalink: true
+        },
+        config: (md) => {
+            md.use(require('markdown-it-katex'))
+            md.use(require('markdown-it-task-lists'))
+            md.options.linkify = true
+        }
+    },
+    themeConfig: {
+        nav: [
+            { text: 'About'        , link: '/about.html' },
+            { text: 'Articles'       , link: '/list.html' },
+            { text: 'Chukapi-Fun-Art', link: '/chukapi_fun_art.html' },
+        ],
+        lastUpdated: true,
+        search: true,
+        displayAllHeaders: true,
+    },
 }
