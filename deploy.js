8e99d462469cdb191acee97f3198cc17ed8c7c0b
task4233 <tks.m1205@gmail.com>
2019-03-20 11:19:02 +0000

    [DELETE] comments

--- deploy.js
+++ deploy.js
@@ -1,11 +1,15 @@
-// masterインスタンスを作成
 var master = require("master");
 
-master.publish("./docs", function (err) {
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        } else {
-            console.log("Successfully Delployed!");
-        }
+master.publish("./docs", {
+    user: {
+        name: "task4233",
+        email: "tks.m1205@gmail.com"
+    }
+}, function (err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    } else {
+        console.log("Successfully Delployed!");
+    }
 });
