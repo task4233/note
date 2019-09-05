2c9902c4816bd225d7abe0d4fae651bdd41b5fd9
task4233 <tks.m1205@gmail.com>
2019-09-05 12:38:33 +0000

    [ADD] style

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,48 +1,30 @@
 module.exports = {
-    locales: {
-        '/': {
-            lang: 'ja',
-            title: 'Note',
-            description: 'Take it easy. But not lazy.'
-        }
-    },
-    ga: 'UA-134364564-2',
-    base: '/note/',
-    dest: 'docs',
-    head: [
-        ['link',
-         {
-             rel: 'stylesheet',
-             href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'
-         }
-        ],
-        ['link',
-         {
-             rel: 'stylesheet',
-             href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css'
-         }
-        ],
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: 'Note',
+      description: 'Take it easy. But not lazy.'
+    }
+  },
+  base: '/note/',
+  dest: 'docs',
+  plugins: [
+    '@vuepress/blog',
+    'latex',
+    '@vuepress/google-analytics',
+    {
+      ga: 'UA-134364564-2',
+    }
+  ],
+  themeConfig: {
+    sidebar: 'auto',
+    sidebarDepth: 2,
+    nav: [
+      { text: 'About'        , link: '/about.html' },
+      { text: 'Articles'       , link: '/list.html' },
+      { text: 'Chukapi-Fun-Art', link: '/chukapi_fun_art.html' },
     ],
-    markdown: {
-        lineNumbers: true,
-        anchor: {
-            permalink: true
-        },
-        config: (md) => {
-            md.use(require('markdown-it-katex'))
-            md.use(require('markdown-it-task-lists'))
-            md.options.linkify = true
-        }
-    },
-    themeConfig: {
-        sidebar: 'auto',
-        sidebarDepth: 2,
-        nav: [
-            { text: 'About'        , link: '/about.html' },
-            { text: 'Articles'       , link: '/list.html' },
-            { text: 'Chukapi-Fun-Art', link: '/chukapi_fun_art.html' },
-        ],
-        lastUpdated: true,
-        search: true,
-    },
+    lastUpdated: true,
+    search: true,
+  }
 }
