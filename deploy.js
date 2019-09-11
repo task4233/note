fb9e3264b06fc7eed6a913f300b73ee1afa44810
task4233 <tks.m1205@gmail.com>
2019-09-11 10:56:16 +0000

    [UPDATE] deploy.js

--- deploy.js
+++ deploy.js
@@ -1,16 +1,16 @@
 var ghpages = require("gh-pages");
 
 ghpages.publish("./docs", {
-    user: {
-        name: "task4233",
-        email: "tks.m1205@gmail.com"
-    },
-    message: "Auto build and deploy [ci skip]"
+  user: {
+    name: "task4233",
+    email: "tks.m1205@gmail.com"
+  },
+  message: "Auto build and deploy [ci skip]"
 }, function(err) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    } else {
-        console.log("Successfully Delployed!");
-    }
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  } else {
+    console.log("Successfully Delployed!");
+  }
 });
