ca7e96efbcb975af61d88ea8ed9305f1337b3a1c
task4233 <tks.m1205@gmail.com>
2021-03-09 23:33:16 +0900

    [STABLE] update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,18 +5,36 @@ module.exports = {
       title: 'Note',
       description: 'Take it easy. But not lazy.',
       head: [
-        ['meta', {name: 'keywords', content: 'task4233, techblog'}],
-        ['meta', {property: 'og:title', content: 'Note'}],
+        ['meta', {
+          name: 'keywords',
+          content: 'task4233, techblog'
+        }],
+        ['meta', {
+          property: 'og:title',
+          content: 'Note'
+        }],
         ['meta',
           {
             name: 'og:description',
             content: 'Take it easy. But not lazy.',
           },
         ],
-        ['meta', { property: 'og:locale', content: 'ja' }],
-        ['meta', { property: 'og:type', content: 'website' }],
-        ['meta', { property: 'og:url', content: 'https://task4233.dev' }],
-        ['meta', { property: 'og:site_name', content: 'task4233 techblog' }],
+        ['meta', {
+          property: 'og:locale',
+          content: 'ja'
+        }],
+        ['meta', {
+          property: 'og:type',
+          content: 'website'
+        }],
+        ['meta', {
+          property: 'og:url',
+          content: 'https://task4233.dev'
+        }],
+        ['meta', {
+          property: 'og:site_name',
+          content: 'task4233 techblog'
+        }],
         [
           'meta',
           {
@@ -29,34 +47,71 @@ module.exports = {
   },
   base: '/',
   head: [
-    ['link', { rel: 'icon', href: '/icon.png' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['link', { rel: 'stylesheet', href: '/main.css'}],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['link', { rel: 'shortcut icon', href: '/favicon.ico'}],
+    ['link', {
+      rel: 'icon',
+      href: '/icon.png'
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#3eaf7c'
+    }],
+    ['link', {
+      rel: 'stylesheet',
+      href: '/main.css'
+    }],
+    ['link', {
+      rel: 'manifest',
+      href: '/manifest.json'
+    }],
+    ['link', {
+      rel: 'shortcut icon',
+      href: '/favicon.ico'
+    }],
   ],
   dest: 'docs',
-  plugins: [
-    ['@vuepress/blog'],
-    ['latex'],
-    ['markdown-it-bar'],
-    ['@vuepress/google-analytics',
-    {
-      ga: 'UA-134364564-2',
-    }],
-    ['@vuepress/search', {
-      searchMaxSuggestions: 5
-    }]
-  ],
   themeConfig: {
     sidebar: 'auto',
+    logo: '/imgs/icon.png',
     sidebarDepth: 2,
-    nav: [
-      { text: 'Links'          , link: '/#links' },
-      { text: 'Articles'       , link: '/list.html' },
-      { text: 'Chukapi-Fun-Art', link: '/chukapi_fun_art.html' },
+    nav: [{
+        text: 'Links',
+        link: '/#links'
+      },
+      {
+        text: 'Articles',
+        link: '/list.html'
+      },
+      {
+        text: 'Chukapi-Fun-Art',
+        link: '/chukapi_fun_art.html'
+      },
     ],
     lastUpdated: true,
     search: true,
-  }
+  },
+  plugins: [
+    ['@vuepress/blog'],
+    ['latex'],
+    ['@vuepress/google-analytics',
+      {
+        ga: 'UA-134364564-2',
+      }
+    ],
+    ['@vuepress/search', {
+      searchMaxSuggestions: 5
+    }],
+    ['smooth-scroll'],
+    ['@vuepress/back-to-top'],
+    ['@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: {
+          "/": {
+            message: "新しいコンテンツがあります。",
+            buttonText: "更新する",
+          },
+        },
+      }
+    ],
+  ],
 }
