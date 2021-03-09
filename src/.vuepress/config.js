8ae75e4a23e0501c0c8a70733f22ac3eb1e53b9a
task4233 <tks.m1205@gmail.com>
2021-03-09 23:38:05 +0900

    add last-updated

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -113,5 +113,14 @@ module.exports = {
         },
       }
     ],
+    ['@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          const moment = require('moment');
+          moment.locale(lang);
+          return moment(timestamp).fromNow();
+        }
+      }
+    ]
   ],
 }
