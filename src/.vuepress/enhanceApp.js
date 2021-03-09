28be0b9b4c30b2ed7a734a622491bf06dc9e78dd
task4233 <tks.m1205@gmail.com>
2021-03-09 20:51:56 +0900

    update enhanceApp

---
+++ src/.vuepress/enhanceApp.js
@@ -0,0 +1,9 @@
+export default ({
+    Vue, 
+    options, // for root Vue instance
+    router, // router instance for the app
+    siteData, // site metadata
+    isServer, // is this enhancement applied in server-rendering or client
+}) => {
+    // apply enhancements to the app
+}
