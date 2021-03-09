44a9906af63277214957452994b3e51716c5221f
task4233 <tks.m1205@gmail.com>
2021-03-09 23:04:40 +0900

    remove pwa

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,14 +8,10 @@ module.exports = {
   },
   base: '/',head: [
     ['link', { rel: 'icon', href: '/icon.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['link', { rel: 'stylesheet', href: "/main.css"}],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico'}],
   ],
   dest: 'docs',
   plugins: [
@@ -26,13 +22,6 @@ module.exports = {
     {
       ga: 'UA-134364564-2',
     }],
-    [
-      '@vuepress/pwa',
-      {
-        serviceWorker: true,
-        updatePopup: true
-      }
-    ],
     ['@vuepress/search', {
       searchMaxSuggestions: 5
     }]
